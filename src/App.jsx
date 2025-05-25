1a1902ce0e7467d909b4baf16e7e178cca7b859e
E-commerce-React-app <09iqrasiddique.com>
2025-05-25 14:37:20 +0500

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import HeroSection from './Components/HeroSection'
-import Navbar from './Components/Navbar'
+// App.js
+import React, { useState, useEffect } from 'react';
+import HeroSection from './Components/HeroSection';
+import { FaMoon, FaSun } from 'react-icons/fa';
+export default function App() {
+  const [darkMode, setDarkMode] = useState(true);
 
-const App = () => {
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+
   return (
-   <>
-     <HeroSection/>
-     {/* <Navbar/> */}
-     </>
-  )
+    <>
+      <button
+        onClick={() => setDarkMode(!darkMode)}
+        className="fixed top-5 right-5   p-2 rounded z-50"
+      >
+        {darkMode ? <FaSun size={20} className=' text-yellow-300' /> : <FaMoon className='text-black' size={20} />}
+      </button>
+      <HeroSection  darkMode={darkMode} />
+    </>
+  );
 }
-
-export default App
